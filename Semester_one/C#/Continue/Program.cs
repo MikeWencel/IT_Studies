890c5a2ce3e03da8d484da3346e2b0ec0ec22e38
MikeWen <wencel.michael@gmail.com>
2019-11-17 16:37:25 +0100

    Arrays

---
+++ Semester_one/C#/Continue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 21;
+            int suma = 0;
+
+            for(int i = 1;i <=n; i++){
+            if(i % 3 != 0)
+                continue; // przejdź do następnego kroku pętli i zignoruj  polecania poniżej
+                suma = suma + i;
+            }
+            Console.WriteLine("Suma liczb podzielnych przez 3 wynosi: " + suma + " dla n = " + n);
+
+        }
+    }
+}
