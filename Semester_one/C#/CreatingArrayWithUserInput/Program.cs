fd60d36a87614a234f270be2e953da855373a851
MikeWen <wencel.michael@gmail.com>
2019-11-17 17:50:32 +0100

    Done

---
+++ Semester_one/C#/CreatingArrayWithUserInput/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CreatingArrayWithUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string odp = Console.ReadLine();
+            int n = int.Parse(odp);
+            int[] tab = new int[n];
+            int i = 0;
+            while(i < n){
+                Console.Write("Podaj element numer " + i + " ");
+                string ans = Console.ReadLine();
+                int a = int.Parse(ans);
+                tab[i] = a;
+                i++;
+                
+            }
+            
+
+                Array.Sort(tab);
+                Array.Reverse(tab);
+            foreach(int element in tab){
+                        Console.WriteLine(element + ",");
+                }
+        }
+    }
+}
