20eea742824ff38da2bf59bdb62587c145b3f7a4
MikeWen <wencel.michael@gmail.com>
2019-11-17 16:51:40 +0100

    Arrays, changing numbers in index

---
+++ Semester_one/C#/Tablice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = {3,6,9,7,12,34,3,2,4,1};
+            int temp = tablica[2];
+            tablica[2] = tablica[8];
+            tablica[8] = temp;
+            foreach(int wartosc in tablica){
+                Console.Write(wartosc + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
