47cc5b3196e51306ea0ce95bbe0f01dc5ed511be
MikeWen <wencel.michael@gmail.com>
2019-11-17 15:33:45 +0100

    Diagonal star, for in for

---
+++ Semester_one/C#/Diagonal Star/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Diagonal_Star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberOne = Console.ReadLine();
+            int n = int.Parse(numberOne);
+            
+        
+
+            for(int i = 1;i <= n;i++){
+                for(int j = 1; j <= i;j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
