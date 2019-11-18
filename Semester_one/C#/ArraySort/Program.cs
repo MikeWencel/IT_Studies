dd66c967febc41df80cfb6771231e9c10af6b582
MikeWen <wencel.michael@gmail.com>
2019-11-18 07:58:46 +0100

    Done

---
+++ Semester_one/C#/ArraySort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tablica = {3,6,9,7,12,34,3,2,4,1};
+            Array.Sort(tablica);
+            foreach(int element in tablica){
+                Console.Write(element + ", ");
+            }
+            Console.WriteLine("");
+        }   
+        // Nauczyć się wszystkich metod sortowania
+    }
+}
