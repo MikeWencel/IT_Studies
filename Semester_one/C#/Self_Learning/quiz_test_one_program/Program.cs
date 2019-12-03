d12a198e903622d72b625145bb5d5ab1c41a092c
MikeWen <wencel.michael@gmail.com>
2019-12-03 10:17:35 +0100

    Done

---
+++ Semester_one/C#/Self_Learning/quiz_test_one_program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace quiz_test_one_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string numberAsString;
+            int number;
+            int average;
+            for(int i =0;i < 3;i++){
+            numberAsString = Console.ReadLine();
+            number = int.Parse(numberAsString);
+            sum = sum + number;
+            average = sum/3;
+            if(i == 2){
+                Console.WriteLine(average);
+            }
+            }
+            
+            
+        }
+    }
+}
