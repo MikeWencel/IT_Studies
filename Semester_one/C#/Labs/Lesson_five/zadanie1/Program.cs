37437da8d58a52455501410e6d1dd9602e547a43
MikeWencel <wencel.michael@gmail.com>
2019-12-22 15:24:14 +0100

    working with struct

---
+++ Semester_one/C#/Labs/Lesson_five/zadanie1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zadanie1
+{
+    class Program
+    {
+        struct samochod {
+            public string marka;
+            public string model;
+            public int rokProdukcji;
+            public int przebieg;
+
+        }
+        static void Main(string[] args)
+        {
+            samochod a;
+                a.marka = "Opel";
+                a.model = "Astra";
+                a.rokProdukcji = 2015;
+                a.przebieg = 30000;
+
+            samochod b;
+                b.marka = "Ford";
+                b.model = "Focus";
+                b.rokProdukcji = 2013;
+                b.przebieg = 50000;    
+
+            Console.WriteLine(a.przebieg + b.przebieg);
+
+            if (b.rokProdukcji < a.rokProdukcji){
+                Console.WriteLine(b.marka + ", " + b.model + ", " + b.rokProdukcji);
+            } else {
+                Console.WriteLine(a.marka + ", " + a.marka +", "+ b.rokProdukcji);
+            }   
+                
+            
+        }
+    }
+}
