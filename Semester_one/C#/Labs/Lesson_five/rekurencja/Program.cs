1d2ee7c771afb3cc26b3f1b923fcc212cd4668e8
MikeWencel <wencel.michael@gmail.com>
2019-12-22 17:50:23 +0100

    rekurencja

---
+++ Semester_one/C#/Labs/Lesson_five/rekurencja/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace rekurencja
+{
+    class Program
+    {
+
+        static int Fib(int n){
+            if(n == 1 || n == 2){
+                return 1;
+            }
+            else
+             return (Fib(n-1) + Fib(n-2));
+            
+        }
+
+        static int Potega(int n, int a){
+            if (a == 0)
+                return 1;
+            else    
+            return n * Potega(n, a -1);
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Fib(10));
+            Console.WriteLine(Potega(2,10));
+            
+        }
+    }
+}
