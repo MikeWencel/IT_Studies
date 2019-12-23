5bc4726d0c0cd471001b47147a9a1850cda42a48
MikeWencel <wencel.michael@gmail.com>
2019-12-23 16:37:41 +0100

    All about conversions

---
+++ Semester_one/C#/Mastering/conversions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123443455;
+            long bigNum = num;
+
+            Console.WriteLine(bigNum);
+
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.37;
+
+            // type conversion
+
+            string myString = myDouble.ToString();
+
+            Console.WriteLine(myString + 13);
+
+            int myInt;
+            //explicit conversion
+            //Casting
+
+            myInt = (int) myDouble;
+
+            System.Console.WriteLine(myInt);
+        }
+    }
+}
