33180e9e58bdaa630f50c6074d23b8f2f1458825
MikeWencel <wencel.michael@gmail.com>
2019-12-23 16:30:33 +0100

    All about strings

---
+++ Semester_one/C#/Mastering/AllAboutStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AllAboutStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kasia";
+            string who = "wife";
+            string myName = "Mike";
+            string secondName = "Mike";
+
+            
+            Console.WriteLine("--------------");
+            Console.WriteLine("To Upper");
+            name.ToUpper();
+            Console.WriteLine("--------------");
+            Console.WriteLine("To Lower");
+            myName.ToLower();
+            Console.WriteLine("Equals() --> False");
+            Console.WriteLine(who.Equals(name));
+            Console.WriteLine("Equals() --> True");
+            Console.WriteLine(myName.Equals(secondName));
+            Console.WriteLine(name + " is my " + who);
+            
+        }
+    }
+}
